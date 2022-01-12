7c38082dbbaa6be9ad1381b5d7729207d3474806
Avinash <a.shenoy@imperial.ac.uk>
2022-01-12 17:58:20 +0000

    extension fixed

--- R/scale_fill_grafify_c.R
+++ R/scale_fill_grafify_c.R
@@ -5,8 +5,7 @@
 #'
 #' Colour palettes available are as follows:
 #' 
-#' \if{latex}{\figure{grafify_palettesv0.2.0.jpg}{options:width=0.5in}}
-#' \if{html}{\out{<div style="text-align: center">}\figure{grafify_palettesv0.2.0.jpg}{options: style="width:750px;max-width:70\%;"}\out{</div>}}
+#' \if{html}{\out{<div style="text-align: center">}\figure{grafify_palettesv020.jpg}{options: style="width:750px;max-width:70\%;"}\out{</div>}}
 #'
 #' 
 #' 
